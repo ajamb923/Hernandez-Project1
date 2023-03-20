55bc00f5500826be8e474ad86f6c486b2966dd07
Amberetu <ajamb923@gmail.com>
2023-03-20 01:15:11 -0400

    reconfigure provider

--- provider.tf
+++ provider.tf
@@ -1,4 +1,23 @@
-# Below is the provider which helps in connecting with AWS Account
+// Provider
 provider "aws" {
   region = "us-east-1"
+  #profile = "default"
 }
+
+
+// Backend
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-923"
+    key            = "terraform-923/terraform.tfstate"
+    region         = "us-east-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-923"
+  }
+ }
+
+
+
+
