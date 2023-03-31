3c8c3be55d33008a075b2f4b3d8c5e04d9f859be
Amberetu <ajamb923@gmail.com>
2023-03-31 18:04:50 -0400

    remove unnecessary

--- provider.tf
+++ provider.tf
@@ -1,23 +1,22 @@
-// Provider
-provider "aws" {
-  region = "us-east-1"
-  #profile = "default"
-}
+# // Provider
+# provider "aws" {
+#   region = "us-east-1"
+#   #profile = "default"
+# }
 
 
-// Backend
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-923"
-    key            = "terraform-923/terraform.tfstate"
-    region         = "us-east-1"
+# // Backend
+# terraform {
+#   backend "s3" {
+#     # Replace this with your bucket name!
+#     bucket         = "terraform-923"
+#     key            = "terraform-923/terraform.tfstate"
+#     region         = "us-east-1"
 
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-923"
-  }
- }
-
+#     # Replace this with your DynamoDB table name!
+#     dynamodb_table = "terraform-923"
+#   }
+#  }
 
 
 
